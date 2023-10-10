707ffc1348c1128b11f3819e3e8b6173797573e2
jason <jason@hedmans.org>
2023-10-10 11:24:10 -0500

    add interfaces

---
+++ types/Review.ts
@@ -0,0 +1,10 @@
+export interface Review {
+    id: string;
+    classId: string;
+    professorId: string;
+    title: string;
+    content: string;
+    rating: number;
+    difficulty: number;
+    score: number;
+}

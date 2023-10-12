7b75e6a17378020f11d18bbe93f78c3a323cb6af
jason <jason@hedmans.org>
2023-10-11 23:27:18 -0500

    add border to autocomplete and modal theme

--- theme/components/index.ts
+++ theme/components/index.ts
@@ -1,13 +1,15 @@
 import Button from './button';
 import Card from "./card";
 import Menu from "./menu";
+import Modal from "./modal";
 
-import { ThemeComponents } from "@chakra-ui/react";
+import {ThemeComponents} from "@chakra-ui/react";
 
 const components: ThemeComponents = {
     Button,
     Card,
-    Menu
+    Menu,
+    Modal
 }
 
 export default components

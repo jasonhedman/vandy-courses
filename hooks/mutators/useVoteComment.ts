995e80c6dd56bff7a0fb771a957728053805bc77
jason <jason@hedmans.org>
2023-11-09 15:11:45 -0600

    refactor generalized voting

--- hooks/mutators/useVoteComment.ts
+++ hooks/mutators/useVoteComment.ts
@@ -1,61 +1,29 @@
-import {useToast} from "@chakra-ui/react";
-
 import useAuth from "@/hooks/auth/useAuth";
 import useCommentVotes from "@/hooks/queries/useCommentVotes";
+import useVote from "@/hooks/mutators/useVote";
 
 import {voteComment} from "@/services/comment";
 import {addCommentVote, updateCommentVote} from "@/services/commentVotes";
-import {getVoteIncrement, getToastMessage} from "@/services/voteUtils";
 
-import {VoteStatus} from "@/types/Vote";
+
+import {VoteInput, VoteStatus} from "@/types/Vote";
 
 // custom hook to handle voting on comments
 const useVoteComment = (reviewId: string, commentId: string) => {
 
-    // gets the current user, preventing users from voting on comments if they are not logged in
     const { user } = useAuth();
 
     // gets the current votes for the comment
     const { votes, loading } = useCommentVotes(user?.uid || "", reviewId, commentId);
 
-    const toast = useToast();
-
-    const handleVote = async (voteType: VoteStatus) => {
-        if (loading || !user) return;
-
-        const alreadyVoted = votes?.length;
-        const currentVoteStatus = alreadyVoted ? votes[0].voteStatus : VoteStatus.NONE;
-        const newStatus = currentVoteStatus === voteType ? VoteStatus.NONE : voteType;
-        const amountIncrement = getVoteIncrement(currentVoteStatus, newStatus);
-
-        const results = await Promise.all([
-            voteComment(reviewId, commentId, amountIncrement),
-            alreadyVoted
-                ? updateCommentVote(reviewId, commentId, votes[0].id, newStatus)
-                : addCommentVote(reviewId, commentId, {
-                    userId: user.uid || "",
-                    voteStatus: newStatus,
-                })
-        ]);
-
-        toast(getToastMessage(results, currentVoteStatus, newStatus));
-    };
-
-    const onUpvote = async () => {
-        await handleVote(VoteStatus.UPVOTED);
-    };
-
-    const onDownvote = async () => {
-        await handleVote(VoteStatus.DOWNVOTED);
-    };
-
-    return {
-        onUpvote,
-        onDownvote,
-        voteStatus: votes?.length ? votes[0].voteStatus : VoteStatus.NONE,
+    return useVote(
+        user?.uid || "",
+        votes,
         loading,
-    }
-
+        (amountIncrement: number) => voteComment(reviewId, commentId, amountIncrement),
+        (voteId: string, voteStatus: VoteStatus) => updateCommentVote(reviewId, commentId, voteId, voteStatus),
+        (vote: VoteInput) => addCommentVote(reviewId, commentId, vote),
+    )
 }
 
 export default useVoteComment;
--- hooks/mutators/useVoteReview.ts
+++ hooks/mutators/useVoteReview.ts
@@ -1,55 +1,28 @@
-import { useToast } from "@chakra-ui/react";
-
 import useAuth from "@/hooks/auth/useAuth";
+import useVote from "@/hooks/mutators/useVote";
 import useReviewVotes from "@/hooks/queries/useReviewVotes";
 
-import { voteReview } from "@/services/reviews";
-import { addReviewVote, updateReviewVote } from "@/services/reviewVotes";
-import { getVoteIncrement, getToastMessage } from "@/services/voteUtils";
+import {voteReview} from "@/services/reviews";
+import {updateReviewVote, addReviewVote} from "@/services/reviewVotes";
 
-import { VoteStatus } from "@/types/Vote";
+import {VoteInput, VoteStatus} from "@/types/Vote";
 
 // custom hook to handle voting on reviews
 const useVoteReview = (reviewId: string) => {
+
     const { user } = useAuth();
+
+    // gets the current votes for the comment
     const { votes, loading } = useReviewVotes(user?.uid || "", reviewId);
-    const toast = useToast();
 
-    const handleVote = async (voteType: VoteStatus) => {
-        if (loading || !user) return;
-
-        const alreadyVoted = votes?.length;
-        const currentVoteStatus = alreadyVoted ? votes[0].voteStatus : VoteStatus.NONE;
-        const newStatus = currentVoteStatus === voteType ? VoteStatus.NONE : voteType;
-        const amountIncrement = getVoteIncrement(currentVoteStatus, newStatus);
-
-        const results = await Promise.all([
-            voteReview(reviewId, amountIncrement),
-            alreadyVoted
-                ? updateReviewVote(reviewId, votes[0].id, newStatus)
-                : addReviewVote(reviewId, {
-                    userId: user.uid || "",
-                    voteStatus: newStatus,
-                })
-        ]);
-
-        toast(getToastMessage(results, currentVoteStatus, newStatus));
-    };
-
-    const onUpvote = async () => {
-        await handleVote(VoteStatus.UPVOTED);
-    };
-
-    const onDownvote = async () => {
-        await handleVote(VoteStatus.DOWNVOTED);
-    };
-
-    return {
-        onUpvote,
-        onDownvote,
-        voteStatus: votes?.length ? votes[0].voteStatus : VoteStatus.NONE,
+    return useVote(
+        user?.uid || "",
+        votes,
         loading,
-    };
+        (amountIncrement: number) => voteReview(reviewId, amountIncrement),
+        (voteId: string, voteStatus: VoteStatus) => updateReviewVote(reviewId, voteId, voteStatus),
+        (vote: VoteInput) => addReviewVote(reviewId, vote),
+    )
 };
 
 export default useVoteReview;

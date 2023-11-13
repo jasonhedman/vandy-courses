60cc0ef41f5ff1195ee03dfd0ac491083ff18db3
jason <jason@hedmans.org>
2023-11-13 16:26:11 -0600

    add labels to input elements

--- components/Utilities/FormComponents/CheckboxInput.tsx
+++ components/Utilities/FormComponents/CheckboxInput.tsx
@@ -5,16 +5,18 @@ import {Checkbox} from "@chakra-ui/react";
 interface Props {
     label: string,
     value: boolean,
+    description?: string,
     onChange: (value: boolean) => void,
     onBlur?: () => void,
     error?: string,
 }
 
-const CheckboxInput: React.FC<Props> = ({ label, value, onChange, onBlur, error }) => {
+const CheckboxInput: React.FC<Props> = ({ label, value, description, onChange, onBlur, error }) => {
     return (
         <FormElement
             label={label}
             error={error}
+            description={description}
         >
             <Checkbox
                 isChecked={value}
